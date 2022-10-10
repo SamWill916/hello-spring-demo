fb1af3b58fd92e2354a9e236db5fc344af622669
S W <sw@Ss-MacBook-Air.local>
2022-10-09 20:25:32 -0500

    controller exercises

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -35,18 +35,79 @@ public class HelloController {
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
+//    @GetMapping("form")
+//    public String helloForm(){
+//        return "<html>" +
+//                "<body>" +
+//                "<form action = 'hello' method = 'post'>" + //submits request to /hello
+//                "<input type = 'text' name = 'name'>" +
+//                "<input type = 'submit' value = 'Greet Me!'>" +
+//                "</form>" +
+//                "</body>" +
+//                "</html>";
+//
+//    }
     @GetMapping("form")
     public String helloForm(){
         return "<html>" +
                 "<body>" +
                 "<form action = 'hello' method = 'post'>" + //submits request to /hello
                 "<input type = 'text' name = 'name'>" +
-                "<input type = 'submit' value = 'Greet Me!'>" +
+                "<select name = \"language\" id=\"language-select\">\n" +
+                    "<option value=\"\">--Please choose an option--</option>" +
+                    "<option value=\"Russian\">Russian</option>\n" +
+                    "<option value=\"Italian\">Italian</option>\n" +
+                    "<option value=\"Spanish\">Spanish</option>\n" +
+                    "<option value=\"French\">French</option>\n" +
+                    "<option value=\"Chinese\">Chinese</option>\n" +
+                "</select>" +
+                "<input type='submit' value = 'Greet Me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
 
     }
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    @ResponseBody
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+        if (name == null) {
+            name = "World";
+        }
+
+        return createMessage(name, language);
+
+        // For a bonus mission, students can change this response text to look nicer.
+        // This is subjective, but students should be modifying the HTML of the response string.
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        }
+        else if (l.equals("french")) {
+            greeting = "Bonjour";
+        }
+        else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+        else if (l.equals("spanish")) {
+            greeting = "Hola";
+        }
+        else if (l.equals("german")) {
+            greeting = "Hallo";
+        }
+        else if (l.equals("chinese")){
+            greeting = "Ni Hao";
+        }
+        else if (l.equals("russian")){
+            greeting = "Privyet";
+        }
+
+        return greeting + " " + n;
+    }
+
 
 }
 
