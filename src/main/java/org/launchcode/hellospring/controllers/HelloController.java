7f81d8091c704853f837a60ac56bf17c266ab0dc
S W <sw@Ss-MacBook-Air.local>
2022-10-09 19:52:19 -0500

    Class Annotations

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.*;
 
 //This is for first 1.1 Section
 @Controller
+@ResponseBody
+@RequestMapping("hello")
 public class HelloController {
 
     //Handles request at path /hello
@@ -14,27 +16,26 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
-    @ResponseBody
+    //lives at /hello/goodbye
+
     @GetMapping("goodbye")
         public String goodbye() {
         return "Goodbye, Spring!";
     }
-    //Handles requests of the form /hello?name=LaunchCode
 
-    //    //Handles requests of the form /hello/LaunchCode
-    @GetMapping("hello")
-    @ResponseBody
+
+    //Handles requests of the form /hello?name=LaunchCode
+    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
     public String helloWithQueryParam(@RequestParam String name){
         return "Hello, " + name + "!";
     }
 
-    @GetMapping("hello/{name}")
-    @ResponseBody
+    //    //Handles requests of the form /hello/LaunchCode
+    @GetMapping("{name}")
     public String helloWithPathParam(@PathVariable String name){
         return "Hello, " + name + "!";
     }
     @GetMapping("form")
-    @ResponseBody
     public String helloForm(){
         return "<html>" +
                 "<body>" +
