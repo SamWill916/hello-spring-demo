42810b58f3c6d94dbda06d2f6db1e6ff553372c7
S W <sw@Ss-MacBook-Air.local>
2022-10-15 18:25:01 -0500

    Fixed ambiguous handler request

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -24,18 +24,19 @@ public class HelloController {
     }
 
 
-    //Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
-    }
+    //Handles requests of the form /hello?name=Sam
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name){
+//        return "Hello, " + name + "!";
+//    }
+//
+//    //    //Handles requests of the form /hello/Sam
+//    @GetMapping("{name}")
+//    public String helloWithPathParam(@PathVariable String name){
+//        return "Hello, " + name + "!";
+//    }
 
-    //    //Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
-    }
-//    @GetMapping("form")
+    //    @GetMapping("form") //this is the old button form
 //    public String helloForm(){
 //        return "<html>" +
 //                "<body>" +
@@ -47,65 +48,65 @@ public class HelloController {
 //                "</html>";
 //
 //    }
-    @GetMapping("form")
+    @GetMapping("form")//lives in /hello/form
     public String helloForm(){
         return "<html>" +
                 "<body>" +
                 "<form action = 'hello' method = 'post'>" + //submits request to /hello
                 "<input type = 'text' name = 'name'>" +
-                "<select name = \"language\" id=\"language-select\">\n" +
-                    "<option value=\"\">--Please choose an option--</option>" +
-                    "<option value=\"Russian\">Russian</option>\n" +
-                    "<option value=\"Italian\">Italian</option>\n" +
-                    "<option value=\"Spanish\">Spanish</option>\n" +
-                    "<option value=\"French\">French</option>\n" +
-                    "<option value=\"Chinese\">Chinese</option>\n" +
+                "<select name = 'language' id='language-select'>" +
+                    "<option value=''>--Please choose an option--</option>" +
+                    "<option value='Russian'>Russian</option>" +
+                    "<option value='Italian'>Italian</option>" +
+                    "<option value='Spanish'>Spanish</option>" +
+                    "<option value='French'>French</option>" +
+                    "<option value='Chinese'>Chinese</option>'" +
                 "</select>" +
                 "<input type='submit' value = 'Greet Me!'>" +
                 "</form>" +
                 "</body>" +
                 "</html>";
 
-    }
-    @RequestMapping(value="hello", method = RequestMethod.POST)
-    @ResponseBody
-    public String helloPost(@RequestParam String name, @RequestParam String language) {
-        if (name == null) {
-            name = "World";
-        }
-
-        return createMessage(name, language);
-
-        // For a bonus mission, students can change this response text to look nicer.
-        // This is subjective, but students should be modifying the HTML of the response string.
-    }
-
-    public static String createMessage(String n, String l) {
+   }
+////    @RequestMapping(value="hello", method = RequestMethod.POST)
+////    @ResponseBody
+////    public String helloPost(@RequestParam String name, @RequestParam String language) {
+////        if (name == null) {
+////            name = "World";
+////        }
+////
+////        return createMessage(name, language);
+//
+//        // For a bonus mission, students can change this response text to look nicer.
+//        // This is subjective, but students should be modifying the HTML of the response string.
+//    }
+@RequestMapping(value ="hello", method = RequestMethod.POST)
+    public static String createMessage(@RequestParam String name, @RequestParam String language) {
         String greeting = "";
 
-        if (l.equals("english")) {
+        if (language.equals("English")) {
             greeting = "Hello";
         }
-        else if (l.equals("french")) {
+        else if (language.equals("French")) {
             greeting = "Bonjour";
         }
-        else if (l.equals("italian")) {
+        else if (language.equals("Italian")) {
             greeting = "Bonjourno";
         }
-        else if (l.equals("spanish")) {
+        else if (language.equals("Spanish")) {
             greeting = "Hola";
         }
-        else if (l.equals("german")) {
+        else if (language.equals("German")) {
             greeting = "Hallo";
         }
-        else if (l.equals("chinese")){
+        else if (language.equals("Chinese")){
             greeting = "Ni Hao";
         }
-        else if (l.equals("russian")){
+        else if (language.equals("Russian")){
             greeting = "Privyet";
         }
 
-        return greeting + " " + n;
+        return greeting + " " + name;
     }
 
 }
