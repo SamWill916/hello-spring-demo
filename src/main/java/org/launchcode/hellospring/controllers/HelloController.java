0616f43856a787691b875b0f4e85ab6f895c0f35
S W <sw@Ss-MacBook-Air.local>
2022-10-15 21:00:40 -0500

    Hello-list

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 //This is for first 1.1 Section
 @Controller
@@ -22,21 +26,34 @@ public class HelloController {
 
 
     //Handles requests of the form /hello?name=Sam
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name){
-        return "Hello, " + name + "!";
+    //changed to interact with hello form to reference greeting variable in template
+    //model.addattrib(First is name on hello.html, second is string above)
+    //need to add Model to parameter list to use model.addAttrib
+    @RequestMapping(value = "hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(@RequestParam String name, Model model){
+        String greeting = "Hello, " + name + "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name){
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model){
+        model.addAttribute("greeting", "Hello, " + name + "!");
+        return "hello";
     }
     @GetMapping("form")
     public String helloForm(){
         return "form";
 //returning form.html - just returning name of template
+    }
+    @GetMapping("hello-names")
+    public String helloNames(Model model){
+        List<String> names = new ArrayList<>();
+        names.add("Bingo");
+        names.add("Bongo");
+        names.add("Bango");
+        model.addAttribute("names", names);
+        return "hello-list";
     }
 
 }
